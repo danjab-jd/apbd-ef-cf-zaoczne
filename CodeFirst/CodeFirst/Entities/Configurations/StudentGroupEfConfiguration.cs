97dc1afc1ea2de758e3c1385de0a6b35dc0e8654
danjab-jd <jd@pja.edu.pl>
2022-05-29 19:27:05 +0200

    CF Example

---
+++ CodeFirst/CodeFirst/Entities/Configurations/StudentGroupEfConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Entities.Configurations
+{
+    public class StudentGroupEfConfiguration : IEntityTypeConfiguration<StudentGroup>
+    {
+        public void Configure(EntityTypeBuilder<StudentGroup> builder)
+        {
+            builder.HasKey(e => new { e.IdStudent, e.IdGroup }).HasName("StudentGroup_pk");
+
+            builder.Property(e => e.AddedAt).IsRequired().HasDefaultValue(DateTime.Now);
+
+
+            builder.HasOne(e => e.IdStudentNavigation)
+                .WithMany(e => e.StudentGroups)
+                .HasForeignKey(e => e.IdStudent)
+                .HasConstraintName("StudentGroup_Student")
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            builder.HasOne(e => e.IdGroupNavigation)
+               .WithMany(e => e.StudentGroups)
+               .HasForeignKey(e => e.IdGroup)
+               .HasConstraintName("StudentGroup_Group")
+               .OnDelete(DeleteBehavior.ClientSetNull);
+
+            builder.ToTable("Student_Group");
+        }
+    }
+}
---
+++ CodeFirst/CodeFirst/Entities/UniversityContext.cs
@@ -0,0 +1,27 @@
+﻿using CodeFirst.Entities.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Entities
+{
+    public class UniversityContext : DbContext
+    {
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Group> Groups { get; set; }
+        public virtual DbSet<StudentGroup> StudentGroups { get; set; }
+
+        public UniversityContext() { }
+
+        public UniversityContext(DbContextOptions<UniversityContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(StudentEfConfiguration).GetTypeInfo().Assembly);
+        }
+    }
+}
