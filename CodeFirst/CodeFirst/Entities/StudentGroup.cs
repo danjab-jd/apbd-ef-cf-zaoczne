8415443892c48e613cf282d5380fe62d2884a529
danjab-jd <jd@pja.edu.pl>
2022-05-29 14:08:10 +0200

    CF Tables

---
+++ CodeFirst/CodeFirst/Entities/StudentGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Entities
+{
+    public class StudentGroup
+    {
+        public int IdStudent { get; set; }
+        public int IdGroup { get; set; }
+        public DateTime AddedAt { get; set; }
+
+        public virtual Student IdStudentNavigation { get; set; }
+        public virtual Group IdGroupNavigation { get; set; }
+    }
+}
---
+++ CodeFirst/CodeFirst/Migrations/20220529120051_AddedCFTables.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeFirst.Migrations
+{
+    public partial class AddedCFTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Group",
+                columns: table => new
+                {
+                    IdGroup = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Group_pk", x => x.IdGroup);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    IdStudent = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    IndexNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Student_pk", x => x.IdStudent);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student_Group",
+                columns: table => new
+                {
+                    IdStudent = table.Column<int>(type: "int", nullable: false),
+                    IdGroup = table.Column<int>(type: "int", nullable: false),
+                    AddedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2022, 5, 29, 14, 0, 51, 51, DateTimeKind.Local).AddTicks(3956))
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("StudentGroup_pk", x => new { x.IdStudent, x.IdGroup });
+                    table.ForeignKey(
+                        name: "StudentGroup_Group",
+                        column: x => x.IdGroup,
+                        principalTable: "Group",
+                        principalColumn: "IdGroup",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "StudentGroup_Student",
+                        column: x => x.IdStudent,
+                        principalTable: "Student",
+                        principalColumn: "IdStudent",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Student_Group_IdGroup",
+                table: "Student_Group",
+                column: "IdGroup");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Student_Group");
+
+            migrationBuilder.DropTable(
+                name: "Group");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+        }
+    }
+}
